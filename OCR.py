8b5173c2f320c713c27216367d4be8f07d5a0cb0
Dallas Kelle <dallas.kelle@gmail.com>
2018-02-09 00:21:12 -0600

    Changing the image type from tiff to jpg, because tiffs allowed for an alpha channel, which messed up the OCR

--- OCR.py
+++ OCR.py
@@ -4,35 +4,46 @@ from threading import Thread, Lock
 import logging
 
 logging.basicConfig(filename='alerts.log',level=logging.INFO)
-PAGES = 6 
-THREADS = 3
+PAGES = 3679
+PAGES = 20
+THREADS = 10
 PAGE_PER_THREAD = PAGES / THREADS
 
-def OCR(thread):
-    for i in range(PAGE_PER_THREAD*thread, PAGE_PER_THREAD*thread+PAGE_PER_THREAD):
+def OCR_on_pages(pages):
+    for i in pages:
+        OCR_one_page(i)
+
+def OCR(thread_number):
+    for i in range(PAGE_PER_THREAD*thread_number, PAGE_PER_THREAD*thread_number+PAGE_PER_THREAD):
+        print 'about to OCR page {}'.format(i)
         page_number = i
+        OCR_one_page(page_number)
 
-        info('starting thread {} at page! {}'.format(thread, page_number))
-        print 'staring thread {} at page {}'.format(thread, page_number)
-        pdf_name = 'wong/wong-page{}.pdf'.format(page_number)
-        tiff_name = 'tiff/img-page{}.tiff'.format(page_number)
-        save_name = 'output/wong{}.txt'.format(page_number)
+def OCR_one_page(page_number):
+    pdf_name = 'wong/wong-page{}.pdf'.format(page_number)
+    jpg_name = 'jpg/img-page{}.jpg'.format(page_number)
+    save_name = 'jpg_output/wong{}.txt'.format(page_number)
 
-        convert_to_tiff_command = "convert -density 300 {} -depth 8 {}".format(pdf_name, tiff_name)
-        ocr_command = "tesseract {} {}".format(tiff_name, save_name)
+    convert_to_jpg_command = "convert -density 300 {} -depth 8 {}".format(pdf_name, jpg_name)
+    ocr_command = "tesseract {} {}".format(jpg_name, save_name)
+    remove_jpg = "rm {}".format(jpg_name)
 
-        try:
-            info('TO TIFF: Thread {} starting...'.format(page_number))
-            process = subprocess.Popen(convert_to_tiff_command.split())
-            output, error = process.communicate()
-            info('output, error {} {}'.format(output, error))
-            info('TO SAVE FILE: Thread {} starting...'.format(page_number))
-            process = subprocess.Popen(ocr_command.split())
-            output, error = process.communicate()
-            info('output, error {} {}'.format(output, error))
-        except Exception as e:
-            exc(e)
-            print 'hit exception'
+    try:
+        info('TO jpg: Thread {} starting...'.format(page_number))
+        process = subprocess.Popen(convert_to_jpg_command.split())
+        output, error = process.communicate()
+        info('output, error {} {}'.format(output, error))
+        info('TO SAVE FILE: Thread {} starting...'.format(page_number))
+        process = subprocess.Popen(ocr_command.split())
+        output, error = process.communicate()
+        info('output, error {} {}'.format(output, error))
+        process = subprocess.Popen(remove_jpg.split())
+        output, error = process.communicate()
+        info('output, error {} {}'.format(output, error))
+        info('REMOVING JPG: {}...'.format(jpg_name))
+    except Exception as e:
+        exc(e)
+        print 'hit exception'
 
 def info(msg):
     with Lock():
@@ -47,7 +58,7 @@ def exc(msg):
 def main():
     print 'hi'
     for i in range(THREADS):
-        Thread(target=OCR, args=(i,)).start()
+        Thread(target=OCR, args=(i+1,)).start()
 
 
 if __name__ == "__main__":
